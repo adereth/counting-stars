2bf6358e005b8a3d73c52e57566cd9fc8dad7608
Matt Adereth <adereth@gmail.com>
2013-12-22 23:08:33 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to counting-stars
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/counting_stars/core_test.clj
@@ -0,0 +1,7 @@
+(ns counting-stars.core-test
+  (:require [clojure.test :refer :all]
+            [counting-stars.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
