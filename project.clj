90ac15b81eab44b28fd3a85aa726e09dd0c07366
Matt Adereth <adereth@gmail.com>
2013-12-22 23:16:28 -0500

    Adding dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject counting-stars "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Data processing for my Counting Stars on Github post"
+  :url "https://github.com/adereth/counting-stars"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [tentacles "0.2.5"]])
